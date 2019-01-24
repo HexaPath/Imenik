3a5244aee38446bbba465443d8e96d13b406d52f
HexaPath <43265617+HexaPath@users.noreply.github.com>
2019-01-24 09:30:57 +0100

    Form Design

--- Imenik/AddEdit.cs
+++ Imenik/AddEdit.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Imenik
 {
-    public partial class Form1 : Form
+    public partial class AddEdit : Form
     {
-        public Form1()
+        public AddEdit()
         {
             InitializeComponent();
         }
---
+++ Imenik/Imenik.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Imenik
+{
+    partial class ImenikForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Ime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priimek = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HisnaStevilka = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Posta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kraj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefonska = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Izberi imenik :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nov imenik :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(366, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ime,
+            this.Priimek,
+            this.HisnaStevilka,
+            this.Posta,
+            this.Kraj,
+            this.Telefonska,
+            this.Mail});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(755, 282);
+            this.dataGridView1.TabIndex = 4; 
+            // 
+            // Ime
+            // 
+            this.Ime.HeaderText = "Ime";
+            this.Ime.Name = "Ime";
+            this.Ime.ReadOnly = true;
+            // 
+            // Priimek
+            // 
+            this.Priimek.HeaderText = "Priimek";
+            this.Priimek.Name = "Priimek";
+            this.Priimek.ReadOnly = true;
+            // 
+            // HisnaStevilka
+            // 
+            this.HisnaStevilka.HeaderText = "Hišna Številka";
+            this.HisnaStevilka.Name = "HisnaStevilka";
+            this.HisnaStevilka.ReadOnly = true;
+            // 
+            // Posta
+            // 
+            this.Posta.HeaderText = "Pošta";
+            this.Posta.Name = "Posta";
+            this.Posta.ReadOnly = true;
+            // 
+            // Kraj
+            // 
+            this.Kraj.HeaderText = "Kraj";
+            this.Kraj.Name = "Kraj";
+            this.Kraj.ReadOnly = true;
+            // 
+            // Telefonska
+            // 
+            this.Telefonska.HeaderText = "Telefonska";
+            this.Telefonska.Name = "Telefonska";
+            this.Telefonska.ReadOnly = true;
+            // 
+            // Mail
+            // 
+            this.Mail.HeaderText = "Mail";
+            this.Mail.Name = "Mail";
+            this.Mail.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Upravljaj osebe";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(543, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Dodaj imenik";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ImenikForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 330);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "ImenikForm";
+            this.Text = "Imenik";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Priimek;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HisnaStevilka;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Posta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kraj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefonska;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mail;
+    }
+}
+

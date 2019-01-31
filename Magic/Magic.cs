12b608485f1399fa69c150040d6f76719b01b5ff
HexaPath <43265617+HexaPath@users.noreply.github.com>
2019-01-31 09:05:30 +0100

    MAGIC

--- Magic/Magic.cs
+++ Magic/Magic.cs
@@ -8,5 +8,33 @@ namespace Magic
 {
     public class Magic
     {
+        public int UserID { get; set; }
+        public int PhoneBookID { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string HomeAdress { get; set; }
+        public int Post { get; set; }
+        public string City { get; set; }
+        public int PhoneNumber { get; set; }
+        public int eMailAddress { get; set; }
+
+        public Magic()
+        {
+
+        }
+        public Magic(int userID)
+        {
+
+        }
+        public Magic(int userID, string name, string username, string homeasress, int post, string city, int phonenumber, int emailaddress)
+        {
+
+        }
+        public Magic(int phonebookid, int userID, string name, string username, string homeasress, int post, string city, int phonenumber, int emailaddress)
+        {
+
+        }
+
+
     }
 }
